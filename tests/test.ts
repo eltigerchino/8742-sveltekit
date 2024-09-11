72f8cfad595cd2e34e5a62dc22790bb833e05467
Tee Ming <chewteeming01@gmail.com>
2024-09-11 20:59:21 +0800

    bump deps and make it work

--- tests/test.ts
+++ tests/test.ts
@@ -1,6 +1,6 @@
-import { expect, test } from '@playwright/test';
-import {getNumber} from "$lib/example";
+import { expect, test } from "@playwright/test";
+import { getNumber } from "$lib/example";
 
-test('index page has expected h1', async ({ page }) => {
-	expect(1).toEqual(getNumber());
+test("index page has expected h1", async ({ page }) => {
+  expect(1).toEqual(getNumber());
 });
